61d2b1b883df0d8d63371ada61a874fbf0a3f19b
Nikola Kolarov <nikolakolarov03@gmail.com>
2022-01-11 23:52:06 +0200

    Add some comments

--- src/versions/sync.rs
+++ src/versions/sync.rs
@@ -2,27 +2,24 @@ use std::fs::OpenOptions;
 use std::io::Write;
 
 pub fn open_file_and_write(path: String) -> Result<(), std::io::Error> {
-    // let mut data = Box::new(String::new());
-    let mut data: Box<String> = Box::new(String::new()); //Box::new(String::new());
-    //let mut data = String::new();
+    // As far as I'm aware, the String type should do a heap allocation under the hood, but
+    // putting it inside a Box<T>(heap allocation) makes me feel smarter, so why not. However
+    // just because you can, doesn't mean you should. But I will do it anyways...
+    let mut data: Box<String> = Box::new(String::new());
+
+    // OpenOptions are something of a 'configuration' on how a file should be open, read and written to.
     let mut file_handle = OpenOptions::new()
         .write(true)
         .append(true)
         .create(true)
         .open(path)?;
 
-    //let mutex_data_for_thread = data.clone();//Arc::clone(&data);
-
     println!("Enter thing to write in file: ");
-    // let thread_handle: JoinHandle<Result<(), std::io::Error>> = thread::spawn( move || {
-    //     let _ = std::io::stdin().read_line(&mut mutex_data_for_thread.lock().unwrap())?; // &mut data
-    //     let _ = writeln!(&mut file_handle, "{}", mutex_data_for_thread.lock().unwrap().as_ref())?;
-    //     return Ok(())
-    // });
 
+    // getting the user's input and writing it to the data.
     let _ = std::io::stdin().read_line(&mut data)?;
+
+    // writing user's input to a file.
     let _ = writeln!(&mut file_handle, "{}", data.as_ref())?;
-    return Ok(())
-    // thread_handle.join().expect("thread::spawn failed")?;
-    // Ok(())
+    Ok(())
 }
